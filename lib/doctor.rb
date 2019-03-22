79c156c41e0090ffe8f9f36e5d09400dc7d8484e
Nataliya R <n.rudenko@gmail.com>
2019-03-22 12:36:55 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,19 @@
+class Doctor
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  @@all = [] 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def new_appointment
+    
+    
+  end 
+  
+  
+end
---
+++ lib/patient.rb
@@ -0,0 +1,16 @@
+class Patient 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  @@all = [] 
+  
+  def self.all 
+    @@all 
+  end
+  
+  
+end 
