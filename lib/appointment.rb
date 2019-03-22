9e9cf10cce9f750bbdb890d555f0c50a498d8e1b
Nataliya R <n.rudenko@gmail.com>
2019-03-22 13:14:12 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment 
-  attr_accessor :date="Monday, August 1st", :patient, :doctor
+  attr_accessor :date=Monday, August 1st, :patient, :doctor
   
-  def initialize (date="Monday, August 1st", patient, doctor)
+  def initialize (date=Monday, August 1st, patient, doctor)
      @date = date
      @patient = patient
      @doctor = doctor
